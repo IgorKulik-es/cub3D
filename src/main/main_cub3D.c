5a59a681fcd2a32ebcd72af609f1fec0f890c776
Viacheslav Trofymenko <vtrofyme@c1r5s3.42madrid.com>
2025-08-22 14:16:10 +0200

    Added maps

--- src/main/main_cub3D.c
+++ src/main/main_cub3D.c
@@ -6,7 +6,7 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 18:54:17 by ikulik            #+#    #+#             */
-/*   Updated: 2025/08/22 00:24:07 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/08/22 12:50:54 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ static void	check_arguments(int argc);
 int	main(int argc, char **argv)
 {
 	t_game	game;
-	int		dummy;
+	// int		dummy;
 
 	check_arguments(argc);
-	if (parse_cub(&game, argv[1]))
-		return (1);
 	initialize_data(&game);
 	create_dummy_map(&game);
 	game.mlx = mlx_init();
+	if (parse_cub(&game, argv[1]))
+		return (1);
 	game.win = mlx_new_window(game.mlx, WIN_WIDTH, WIN_HEIGHT, "cub3D");
-	game.screen.img = mlx_new_image(game.mlx, game.screen.win_w, game.screen.win_h);
-	game.screen.pixels = (int *)mlx_get_data_addr(game.screen.img, &dummy, &dummy, &dummy);
-	mlx_hook(game.win, 2, 1L << 0, key_press, &game);
-	mlx_hook(game.win, 3, 1L << 0, key_release, &game);
-	mlx_hook(game.win, 17, 0L, close_game, &game);
-	mlx_loop_hook(game.mlx, render_frame, &game);
-	mlx_loop(game.mlx);
+	// game.screen.img = mlx_new_image(game.mlx, game.screen.win_w, game.screen.win_h);
+	// game.screen.pixels = (int *)mlx_get_data_addr(game.screen.img, &dummy, &dummy, &dummy);
+	// mlx_hook(game.win, 2, 1L << 0, key_press, &game);
+	// mlx_hook(game.win, 3, 1L << 0, key_release, &game);
+	// mlx_hook(game.win, 17, 0L, close_game, &game);
+	// mlx_loop_hook(game.mlx, render_frame, &game);
+	// mlx_loop(game.mlx);
 }
 
 static void	check_arguments(int argc)
